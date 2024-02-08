08e22183629abcc46f94ea58cbeba187e4d456d5
Dxeg0o <dsoler.olguin@gmail.com>
2024-02-08 00:42:08 -0300

    lol

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -5,6 +5,9 @@ self.__RSC_CSS_MANIFEST={
     ],
     "/Users/diegosolerolguin/Documents/programming/udemy/udemy/app/restaurant/[slug]/page": [
       "/Users/diegosolerolguin/Documents/programming/udemy/udemy/app/globals.css"
+    ],
+    "/Users/diegosolerolguin/Documents/programming/udemy/udemy/app/restaurant/[slug]/menu/page": [
+      "/Users/diegosolerolguin/Documents/programming/udemy/udemy/app/globals.css"
     ]
   },
   "/Users/diegosolerolguin/Documents/programming/udemy/udemy/app/layout.tsx": [
--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e83761f65314cc78")
+/******/ 		__webpack_require__.h = () => ("47690955e635425d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
